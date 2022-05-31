131af6e44c809cfb8213daed434eff1ebc6f443b
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-05-31 23:35:20 +0500

    feat: fs/read task done

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,15 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 export const read = async () => {
-    // Write your code here 
+  const error = new TypeError('FS operation failed');
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  fs.readFile(path.resolve(__dirname, './files', 'fileToRead.txt'), (err, data) => {
+    if (err) throw error;
+    console.log(data.toString());
+  });
 };
+
+read();
