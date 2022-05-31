08631b121463a2071b9ea8f2a2e9777be6fb6d53
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-05-31 23:56:14 +0500

    feat: fs/list task done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,20 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 export const list = async () => {
-    // Write your code here 
+  const error = new TypeError('FS operation failed');
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  fs.readdir(path.join(__dirname, 'files'), (err, data) => {
+    if (err) throw error;
+    const nameFilesArr = [];
+    data.forEach((item) => {
+      nameFilesArr.push(item.split('.').shift());
+    });
+    console.log(nameFilesArr);
+  });
 };
+
+list();
