1a0dc2638592026e339eed6404509259b16dd54d
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-06-01 03:11:01 +0500

    feat: fs/rename task done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,17 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 export const rename = async () => {
-    // Write your code here 
+  const error = new TypeError('FS operation failed');
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const mainFileName = path.join(__dirname, 'files', 'wrongFilename.txt');
+
+  fs.rename(mainFileName, path.join(__dirname, 'files', 'properFilename.md'), (err) => {
+    if (err) throw error;
+    console.log('file was renamed');
+  });
 };
+
+rename();
