a8022843cf02ca7c309872a57f5d13b10dd4a4d1
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-06-01 02:52:05 +0500

    feat: fs/delete task done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,17 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 export const remove = async () => {
-    // Write your code here 
+  const error = new TypeError('FS operation failed');
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const delFile = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+  fs.unlink(delFile, (err) => {
+    if (err) throw error;
+    console.log('file was removed');
+  });
 };
+
+remove();
