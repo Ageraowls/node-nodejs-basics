f320fd51d83ff0055ed80b11c94dcea8a7c161d3
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-06-01 00:36:30 +0500

    feat: fs/create task done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,20 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 export const create = async () => {
-    // Write your code here 
+  const error = new TypeError('FS operation failed');
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const pathToFilesFolder = path.join(__dirname, 'files', 'fresh.txt');
+  fs.stat(pathToFilesFolder, async (err) => {
+    if (!err) throw error;
+    else if (err.code === 'ENOENT') {
+      fs.writeFile(pathToFilesFolder, 'I am fresh and young', () => {
+        console.log('file created');
+      });
+    }
+  });
 };
+
+create();
