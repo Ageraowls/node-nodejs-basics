e67b71407e839c10db937d2149e1be97af5d6186
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-06-02 02:50:13 +0500

    feat: cli/args task done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,17 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const firstValue = [];
+  const secondValue = [];
+  args.forEach((item) => {
+    const str = item.replace(/[-]/g, '');
+    if (firstValue.length < 2) {
+      firstValue.push(str);
+    } else {
+      secondValue.push(str);
+    }
+  });
+
+  console.log(`${firstValue[0]} is ${firstValue[1]}, ${secondValue[0]} is ${secondValue[1]}`);
 };
+
+parseArgs();
