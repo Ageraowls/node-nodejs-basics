d820ce9a36536adf3382dd50e0e2dfc355462ded
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-06-01 22:06:59 +0500

    feat: streams/transform task done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,16 @@
+import { stdin, stdout } from 'process';
+import { pipeline, Transform } from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+  const transformat = new Transform({
+    transform(chunk, enc, cb) {
+      const chunkToStr = chunk.toString().trim();
+      const reverseChunk = chunkToStr.split('').reverse().join('');
+
+      cb(null, `${reverseChunk}\n`);
+    },
+  });
+  pipeline(stdin, transformat, stdout, (err) => console.log(`Error ${err}`));
 };
+
+transform();
