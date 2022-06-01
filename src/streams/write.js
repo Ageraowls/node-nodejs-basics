5566f6e8793ab6d1532f5a20aa0e8d50f000b97c
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-06-01 20:36:01 +0500

    feat: streams/write task done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,23 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { stdin, stdout } from 'process';
+
 export const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const writeableStream = fs.createWriteStream(path.join(__dirname, 'files', 'fileToWrite.txt'));
+
+  stdout.write('send some text:\n');
+  stdin.on('data', (data) => {
+    const str = data;
+    writeableStream.write(str);
+  });
+
+  process.on('exit', () => {
+    stdout.write('\nbye-bye\n');
+  });
+  process.on('SIGINT', () => process.exit());
 };
+
+write();
