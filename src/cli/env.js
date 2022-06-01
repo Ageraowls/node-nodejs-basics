da339a8bf9e4d1a8f36fdfd4e598b7d92c1976f9
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-06-02 01:53:40 +0500

    feat: cli/env task done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,17 @@
+import { env } from 'process';
+
 export const parseEnv = () => {
-    // Write your code here 
+  const keysArr = [];
+  env.RSS_name1 = 'value1';
+  env.RSS_name2 = 'value2';
+
+  for (const obj in env) {
+    if (obj.includes('RSS_')) {
+      keysArr.push(`${obj}=${env[obj]}`);
+    }
+  }
+
+  console.log(keysArr.join('; '));
 };
+
+parseEnv();
