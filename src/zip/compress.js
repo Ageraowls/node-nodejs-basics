50e333a04c84dc6d842b814764a23b3f93eafec1
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-06-01 23:09:00 +0500

    feat: zip/compress task done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,18 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 export const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const mainPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const readStream = fs.createReadStream(mainPath);
+  const writeStream = fs.createWriteStream(mainPath);
+  const compressStream = zlib.createGzip();
+
+  readStream.pipe(compressStream).pipe(writeStream);
+  writeStream.on('finish', () => console.log('file was compressed'));
 };
+
+compress();
