60531a7182fc0b962bcd4555aee7215ca5e107b8
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-06-06 00:33:21 +0500

    add fs:copy script in package.json

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,14 @@
-export const performCalculations = async () => {
-    // Write your code here
-};
+// export const performCalculations = async () => {
+//     // Write your code here
+// };
+
+import os from 'os';
+import { Worker } from 'worker_threads';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+console.log('I am index!');
+
+const worker = new Worker(path.join(__dirname, 'worker.js'));
